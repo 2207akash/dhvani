a591cf7514bfd735cd4cf44e2aff68682325875c
2207akash <2207akash@gmail.com>
2020-06-01 21:53:49 +0530

    Finished styling the navbar

--- index.php
+++ index.php
@@ -24,6 +24,28 @@
 					<a class="logo" href="index.php">
 						<img src="assets/images/icons/logo.png">
 					</a>
+
+					<div class="group">
+						<div class="navItem">
+							<a class="navItemLink" href="search.php">Search
+								<img class="icon" src="assets/images/icons/search.png" alt="Search">
+							</a>
+						</div>
+					</div>
+
+					<div class="group">
+						<div class="navItem">
+							<a class="navItemLink" href="browse.php">Browse</a>
+						</div>
+						
+						<div class="navItem">
+							<a class="navItemLink" href="yourMusic.php">Your music</a>
+						</div>
+
+						<div class="navItem">
+							<a class="navItemLink" href="profile.php">Akash Sen</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
