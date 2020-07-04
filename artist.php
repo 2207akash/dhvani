8257c9427029af03f60984cbb562fed2fe85c85d
2207akash <2207akash@gmail.com>
2020-07-04 14:43:37 +0530

    Added the artist.php page and styled it, added code to select songs or albums by the artist

--- artist.php
+++ artist.php
@@ -1,56 +1,70 @@
-<?php
+<?php 
+
 	include("includes/includedFiles.php");
 
 	if(isset($_GET['id']))
-		$albumId = $_GET['id'];
+		$artistId = $_GET['id'];
 	else
 		header("Location: index.php");
 
-	$album = new Album($con, $albumId);
-	$artist = $album->getArtist();
+	$artist = new Artist($con, $artistId);
 ?>
 
-<div class="entityInfo">
+<div class="entityInfo borderBottom">
+	
+	<div class="centerSection">
+		
+		<div class="artistInfo">
+			<h1 class="artistName"><?php echo $artist->getName(); ?></h1>
 
-	<div class="leftSection">
-		<img src="<?php echo $album->getArtworkPath(); ?>">
-	</div>
+			<div class="headerButtons">
+				<button class="button purple">PLAY</button>
+			</div>
+		</div>
 
-	<div class="rightSection">
-		<h2><?php echo $album->getTitle(); ?></h2>
-		<p>By <?php echo $artist->getName(); ?></p>
-		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
 	</div>
 
 </div>
 
-<div class="trackListController">
+<div class="trackListController borderBottom">
+	<h2>SONGS</h2>
 	<ul class="trackList">
 		<?php  
-			$songIdArray = $album->getSongIds();
+			$songIdArray = $artist->getSongIds();
 
 			$i = 1;
 			foreach($songIdArray as $songId) {
+				if($i > 5)
+					break;
+				
 				$albumSong = new Song($con, $songId);
 				$albumArtist = $albumSong->getArtist();
 
 				echo "<li class='trackListRow'>
 						<div class='trackCount'>
+
 							<img class='play' src='assets/images/icons/play-purple.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
 							<span class='trackNumber'>$i</span>
+
 						</div>
 
 						<div class='trackInfo'>
+
 							<span class='trackName'>" . $albumSong->getTitle() . "</span>
 							<span class='artistName'>" . $albumArtist->getName() . "</span>
+
 						</div>
 
 						<div class='trackOptions'>
+
 							<img class='optionsButton' src='assets/images/icons/more.png'>
+
 						</div>
 
 						<div class='trackDuration'>
+
 							<span class='duration'>" . $albumSong->getDuration() . "</span>
+
 						</div>
 
 					</li>";
@@ -64,4 +78,24 @@
 			tempPlaylist = JSON.parse(tempSongIds);
 		</script>
 	</ul>
+</div>
+
+<div class="gridViewContainer">
+	<h2>ALBUMS</h2>
+	<?php 
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistId'");
+
+		while($row = mysqli_fetch_array($albumQuery)) {
+			echo "<div class='gridViewItem'>
+					<span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
+						<img src='" . $row['artworkPath'] . "'>
+
+						<div class='gridViewInfo'>"
+							. $row['title'] .
+						"</div>
+					</span>
+				</div>";
+		}
+	?>
+
 </div>
