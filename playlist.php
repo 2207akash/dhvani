016a6fdf0957f49455ea9db57a85548657b02bda
2207akash <2207akash@gmail.com>
2020-07-06 01:23:50 +0530

    Implemented the "delete playlist" functionality and finished styling the playlist.php page [TESTED]

--- playlist.php
+++ playlist.php
@@ -22,7 +22,7 @@
 		<h2><?php echo $playlist->getName(); ?></h2>
 		<p>By <?php echo $playlist->getOwner(); ?></p>
 		<p><?php echo $playlist->getNumberOfSongs(); ?> songs</p>
-		<button class="button purple">DELETE PLAYLIST</button>
+		<button class="button delete" onclick="deletePlaylist('<?php echo $playlistId ?>')">DELETE PLAYLIST</button>
 	</div>
 
 </div>
